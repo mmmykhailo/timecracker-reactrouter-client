106fb66a1d8be9074b84e916efd966c33ae04f07
Mykhailo <mmmykhailo@proton.me>
2025-01-09 23:56:21 +0200

    add welcome page

---
+++ app/routes/welcome.tsx
@@ -0,0 +1,46 @@
+import { Link, useNavigate } from "react-router";
+import { Button } from "~/components/ui/button";
+
+import { set as idbSet } from "idb-keyval";
+
+export function meta() {
+  return [
+    { title: "Timecracker" },
+    { name: "description", content: "Stupidly simple timetracker" },
+  ];
+}
+
+export default function WelcomePage() {
+  const navigate = useNavigate();
+
+  const handleOpenFolder = async () => {
+    const rootHandle: FileSystemDirectoryHandle =
+      await window.showDirectoryPicker();
+
+    await idbSet("rootHandle", rootHandle);
+
+    return navigate("/");
+  };
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col items-center gap-4">
+              <img className="w-12 h-12" src="/logo.svg" alt="Timecracker" />
+              <h1 className="text-xl font-bold">Welcome to Timecracker</h1>
+            </div>
+            <Button type="submit" className="w-full" onClick={handleOpenFolder}>
+              Select folder
+            </Button>
+          </div>
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
+            By clicking "Select folder", you agree to my{" "}
+            <Link to="/terms-and-conditions">Terms and Conditions</Link>.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
