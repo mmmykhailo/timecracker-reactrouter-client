da3b443b5ca6043bbca0a8b2044912dd5ac27056
mmmykhailo <35040944+mmmykhailo@users.noreply.github.com>
2025-01-26 00:21:22 +0000

    add loader to stats page

--- app/routes/stats.tsx
+++ app/routes/stats.tsx
@@ -1,8 +1,10 @@
 import { get as idbGet, del as idbDel } from "idb-keyval";
 import { AppHeader } from "~/components/app-header";
 import { readReports } from "~/lib/reports";
-import { redirect, useLoaderData } from "react-router";
+import { Await, redirect, useLoaderData } from "react-router";
 import { YearlyProjectHoursChart } from "~/components/stats/yearly-project-hours-chart";
+import { Suspense } from "react";
+import { LoaderCircle } from "lucide-react";
 
 export function meta() {
   return [
@@ -21,7 +23,7 @@ export async function clientLoader() {
     }
 
     return {
-      reports: await readReports(rootHandle),
+      reports: readReports(rootHandle),
     };
   } catch (e) {
     console.error(e);
@@ -36,9 +38,21 @@ export default function Home() {
   return (
     <div className="min-w-[640px]">
       <AppHeader />
-      <div className="flex flex-col lg:grid lg:grid-cols-12 flex-1 gap-4 p-4 mt-8">
-        <YearlyProjectHoursChart reports={loaderReports} />
-      </div>
+      <Suspense
+        fallback={
+          <div className="grid place-items-center min-h-[50svh]">
+            <LoaderCircle className="w-8 h-8 animate-spin" />
+          </div>
+        }
+      >
+        <Await resolve={loaderReports}>
+          {(reports) => (
+            <div className="flex flex-col lg:grid lg:grid-cols-12 flex-1 gap-4 p-4 mt-8">
+              <YearlyProjectHoursChart reports={reports} />
+            </div>
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
 }
